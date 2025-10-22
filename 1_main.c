a50ed36b7a0ebfdd10e88bee0db918f168e64c5e
苏打NGC2237 <star32349@outlook.com>
2025-10-22 14:28:28 +0800

    提交1
    Modified the program to convert Celsius to Fahrenheit.

--- 1_main.c
+++ 1_main.c
@@ -1,9 +1,12 @@
-//
-//
-//
+//202210116122
+//3093236313@qq.com
+//何小小
 #include <stdio.h>
 int main()
-{
-    printf("Hello World");
+{   
+    float a,b;
+    scanf("%f",&a);
+    b = a*9/5 + 32;
+    printf("%.1f",b);
     return 0;
 }
